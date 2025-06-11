db033cf4d9af3973a79e877ea0e9a7103484bfbd
Pat Manning <patmanning@google.com>
2025-06-11 16:09:17 +0100

    Do not update desktop button page scrolls to prevent snapping.
    Prevent snapping to desktop button by checking during getNextPage, and getting an alternate page from the desktop button with the same scroll.
    
    Fix: 409280640
    Test: Manual
    Flag: EXEMPT Bugfix
    Change-Id: I862ffdfa4f8b59ae32c2fcc21823ad92d70d891f

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -4763,6 +4763,15 @@ public abstract class RecentsView<
         return getTaskViewAt(getNextPage());
     }
 
+    @Override
+    public int getNextPage() {
+        int nextPage = super.getNextPage();
+        if (mDisallowScrollToAddDesk && getPageAt(nextPage) instanceof AddDesktopButton) {
+            nextPage = mUtils.getAlternatePageWithSameScroll(nextPage);
+        }
+        return nextPage;
+    }
+
     protected int getCurrentPageScrollDiff() {
         return mCurrentPageScrollDiff;
     }
@@ -6234,15 +6243,7 @@ public abstract class RecentsView<
         if (addDesktopButtonIndex >= 0 && addDesktopButtonIndex < outPageScrolls.length) {
             int firstViewIndex = getFirstViewIndex();
             if (firstViewIndex >= 0 && firstViewIndex < outPageScrolls.length) {
-                // If we can scroll to [AddDesktopButton], make its page scroll equal to
-                // the first [TaskView]. Otherwise, make its page scroll out of range of
-                // [minScroll, maxScroll].
-                if (!mDisallowScrollToAddDesk) {
-                    outPageScrolls[addDesktopButtonIndex] = outPageScrolls[firstViewIndex];
-                } else {
-                    outPageScrolls[addDesktopButtonIndex] =
-                            outPageScrolls[firstViewIndex] + (mIsRtl ? 1 : -1);
-                }
+                outPageScrolls[addDesktopButtonIndex] = outPageScrolls[firstViewIndex];
             }
 
             if (DEBUG) {
