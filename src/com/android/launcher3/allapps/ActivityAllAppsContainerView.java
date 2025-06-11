b2a3e2ac4f942fe974bc34edb3c88e5b1a598930
Brandon Dayauon <brdayauon@google.com>
2025-06-11 11:23:25 -0700

    Fix inconsistent bottom padding in work tab.
    Was able to repro when the workUtilityView was collapsed/expanded. When collapsed, the height returned during applyPadding() in mWorkManager.getWorkUtilityView().getHeight() is inconsistent compared to when it is expanded. For consistency, the child view's measuredHeight (workFAB and workScheduler) should be used in calculating the total height.
    
    bug: 410012332
    Test: manually.
    Flag: EXEMPT bug fix
    Change-Id: I624a79b41349e36c83c22c992a1942b649f58c3c

--- src/com/android/launcher3/allapps/ActivityAllAppsContainerView.java
+++ src/com/android/launcher3/allapps/ActivityAllAppsContainerView.java
@@ -1677,7 +1677,8 @@ public class ActivityAllAppsContainerView<T extends Context & ActivityContext>
             if (mRecyclerView != null) {
                 int bottomOffset = 0;
                 if (isWork() && mWorkManager.getWorkUtilityView() != null) {
-                    bottomOffset = mInsets.bottom + mWorkManager.getWorkUtilityView().getHeight();
+                    bottomOffset =
+                            mInsets.bottom + mWorkManager.getWorkUtilityView().getTotalHeight();
                 } else if (isMain() && mPrivateProfileManager != null) {
                     Optional<AdapterItem> privateSpaceHeaderItem = mAppsList.getAdapterItems()
                             .stream()
