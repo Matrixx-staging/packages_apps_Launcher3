4bd120b55efb0afc3ffbd144464484b5a8c0d834
Uwais Ashraf <uwaisashraf@google.com>
2025-08-07 14:35:01 +0000

    Clear mActiveGestureGroupedTaskInfo as it can sometimes be left set if onGestureAnimationEnd is not called after onGAStart.
    Fix: 437066331
    Flag: EXEMPT - bugfix
    Test: None
    Change-Id: I15641ee20269daca9ff78cdd92f98f8d84bc968c

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -2771,6 +2771,7 @@ public abstract class RecentsView<
         setFocusedTaskViewId(INVALID_TASK_ID);
         mAnyTaskHasBeenDismissed = false;
         setTaskIconVisible(true);
+        mActiveGestureGroupedTaskInfo = null;
         if (mAddDesktopButton != null) {
             mAddDesktopButton.setGestureAlpha(1f);
         }
