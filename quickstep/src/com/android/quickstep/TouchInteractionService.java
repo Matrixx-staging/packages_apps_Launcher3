cc86657801a0ed4a4f75f0ce6226eb9a64989da3
Sergey Pinkevich <spinkevich@google.com>
2025-08-28 14:39:03 +0000

    Reject Home animation on Launcher side
    Bug: 427579352
    Flag: com.android.window.flags.enable_launcher_handle_go_home_keyboard_shortcut
    Test: manually
    
    Change-Id: I820f75b37788cc7086bddeb98dc9d4d84333e01c

--- quickstep/src/com/android/quickstep/TouchInteractionService.java
+++ quickstep/src/com/android/quickstep/TouchInteractionService.java
@@ -66,6 +66,7 @@ import android.view.Display;
 import android.view.InputDevice;
 import android.view.InputEvent;
 import android.view.MotionEvent;
+import android.window.DesktopExperienceFlags;
 import android.window.DesktopExperienceFlags.DesktopExperienceFlag;
 
 import androidx.annotation.BinderThread;
@@ -909,6 +910,10 @@ public class TouchInteractionService extends Service {
         mAllAppsActionManager.onUserUnlocked();
         mQuickstepKeyGestureEventsHandler.registerOverviewKeyGestureEvent(
                 createOverviewGestureHandler());
+        if (DesktopExperienceFlags.ENABLE_LAUNCHER_HANDLE_GO_HOME_KEYBOARD_SHORTCUT.isTrue()) {
+            mQuickstepKeyGestureEventsHandler.registerHomeKeyGestureEvent(
+                    getOverviewCommandHelper());
+        }
     }
 
     public OverviewCommandHelper getOverviewCommandHelper() {
