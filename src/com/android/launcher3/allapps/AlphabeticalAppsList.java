8b2c4f7134ee2f448c482f5303156737edbc4aa8
Brandon Dayauon <brdayauon@google.com>
2025-09-04 18:53:05 -0700

    Make private space package public
    bug: 443084727
    Test: video in bug
    Flag: EXEMPT bug fix
    Change-Id: I58a020f0e14d50b0c5923209d3c11d8f186089eb

--- src/com/android/launcher3/allapps/AlphabeticalAppsList.java
+++ src/com/android/launcher3/allapps/AlphabeticalAppsList.java
@@ -60,7 +60,7 @@ import java.util.stream.Stream;
 public class AlphabeticalAppsList implements AllAppsStore.OnUpdateListener {
 
     public static final String TAG = "AlphabeticalAppsList";
-    private static final String PRIVATE_SPACE_PACKAGE = "com.android.privatespace";
+    public static final String PRIVATE_SPACE_PACKAGE = "com.android.privatespace";
 
     private final WorkProfileManager mWorkProviderManager;
 
